5d0a31370ba171dba5c5ea29b7fe0eac0d005ab3
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-18 16:31:08 +0100

    Basic working version of widget

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import ToggleSwitch from "./components/toggleSwitch/ToggleSwitch";
+import { FaPlus, FaMinus, FaBatteryEmpty } from "react-icons/fa";
+
+export type OptionLights = "none" | "NV" | "DtD" | "Flashing";
 
 export const Widget = () => {
+   const opArr: OptionLights[] = ["none", "NV", "DtD", "Flashing"]
    const percentArr = ["1%", "3%", "10%", "30%", "100%"];
    const [count, setCount] = useState(0);
+   const [optionLight, setOptionLight] = useState<OptionLights>("none");
 
    return (
       <div className="widget">
@@ -13,22 +19,47 @@ export const Widget = () => {
             </div>
             <div className="stepper">
                {/* <Stepper /> */}
-               --- --- --- --- ---
+               <span className={`bar ${count >= 0 ? "bar--light" : ""}`} />
+               <span className={`bar ${count >= 1 ? "bar--light" : ""}`} />
+               <span className={`bar ${count >= 2 ? "bar--light" : ""}`} />
+               <span className={`bar ${count >= 3 ? "bar--light" : ""}`} />
+               <span className={`bar ${count >= 4 ? "bar--light" : ""}`} />
             </div>
             <div className="plusminus-container">
                {/* <PlusMinusButtons /> */}
-               <button onClick={() => count < 4 && setCount(count + 1)}>+</button>
-               <div>
+               <button
+                  className="btn btn--blue"
+                  onClick={() => count < 4 && setCount(count + 1)}>
+                  <FaPlus />
+               </button>
+               <div className="percentbox">
                   {percentArr[count]}
                </div>
-               <button onClick={() => count > 0 && setCount(count - 1)}>-</button>
+               <button
+                  className="btn btn--gray"
+                  onClick={() => count > 0 && setCount(count - 1)}>
+                  <FaMinus />
+               </button>
             </div>
             <div className="main-container">
-               {/* <RightSide /> */}
-               <div>Time left</div>
-               <div>Night Vision</div>
-               <div>Dusk Till Dawn</div>
-               <div>Flashing</div>
+               <div className="timeleft">
+                  <p><FaBatteryEmpty /> Time left</p>
+               </div>
+               <ToggleSwitch
+                  name={opArr[1]}
+                  optionLight={optionLight}
+                  setOptionLight={setOptionLight}
+                  label="Night Vision" />
+               <ToggleSwitch
+                  name={opArr[2]}
+                  optionLight={optionLight}
+                  setOptionLight={setOptionLight}
+                  label="Dusk Till Dawn" />
+               <ToggleSwitch
+                  name={opArr[3]}
+                  optionLight={optionLight}
+                  setOptionLight={setOptionLight}
+                  label="Flashing" />
             </div>
          </div>
       </div>
