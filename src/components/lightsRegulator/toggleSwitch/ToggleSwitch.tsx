55a0d559cab8fca83020e1047ec618cd0d2883c0
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-19 20:19:09 +0100

    Add blink after 5 secunds

--- src/components/lightsRegulator/toggleSwitch/ToggleSwitch.tsx
+++ src/components/lightsRegulator/toggleSwitch/ToggleSwitch.tsx
@@ -2,23 +2,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeLightType } from "../../../actions";
 import { WidgetState } from "../../../widgetReducer";
 import { LightState, OptionLights } from "../types";
+import { useState, useEffect } from 'react';
 import "./ToggleSwitch.css";
 
 interface props {
-   label: string, 
-   name: OptionLights, 
+   label: string,
+   name: OptionLights,
 }
 
 const ToggleSwitch = ({ label, name }: props) => {
    const dispatch = useDispatch();
    const optionLight = useSelector<WidgetState, WidgetState["selectedLight"]>(state => state.selectedLight);
-   
+
+   const [isShown, setIsShown] = useState(false);
+
+   useEffect(() => {
+      if (name === optionLight) {
+         setTimeout(() => setIsShown(true), 5000);
+      } else {
+         setIsShown(false);
+      }
+
+   }, [optionLight]);
+
    const onChangeLightType = (light: string) => {
       dispatch(changeLightType(light));
    }
 
    return (
-      <div className="toggle-container">
+      <div className={`toggle-container ${isShown ? "blink" : ""}`}>
          {label}
          <div className="toggle-switch">
             <input
